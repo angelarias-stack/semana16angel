e184b82536c3fd033fcb674d4d3785b0d91fd26f
Ángel Arias <ariasdelcarpioangelherber@gmail.com>
2026-07-03 19:57:04 -0500

    Agregar ruta raíz

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend funcionando ✅');
+});
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
